8d6955b02fe9b2db5f7d3afaf4cee69117fefadd
Peter <peterhoang2704@gmail.com>
2025-01-31 17:02:23 -0600

    Implemented BubbleSort class using SortingAlgorithm interface, which takes in any input (array).

---
+++ src/BubbleSortClass.java
@@ -0,0 +1,18 @@
+public class BubbleSortClass implements SortingAlgorithm {
+    @Override
+    public int[] sorty(int[] input) {
+        int n = input.length;
+        int[] a = input.clone();
+        int tmp;
+        for (int i = 0; i < n - 1; i++) { // number of elements sorted
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (a[j + 1] < a[j]) { // compare neighbors
+                    tmp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = tmp;
+                }
+            }
+        }
+        return a;
+    }
+}
